f4bac5c3d6e966caecc77e3cbf938c914b9beb67
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-17 15:21:46 +0200

    muutoksia

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
-$db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','root');
+$db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
 $sql = "select * from item";
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
