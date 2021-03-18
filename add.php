96a7cc1a791ce65977392c32d3693b4806726a9b
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-18 17:46:47 +0200

    Muutoksia

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Accept, Content-Type,Access-Control-Allow-Header');
 header('Content-type: application/json');
 header('Access-Control-Max-Age: 3600');
@@ -10,14 +10,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     return 0;
 }
 
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+
 try {
     $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $sql = "select * from item";
-    $query = $db->query($sql);
-    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $query = $db->prepare('insert into item(description) values (:description)');
+    $query->bindValue(':description',$description,PDO::PARAM_STR);
+    $query->execute();
+
     header('HTTP/1.1 200 OK');
-    echo json_encode($results);
+    $data = array('id' => $db->lastInsertId(),'description' => $description);
+    echo json_encode($data);
 }
 catch (PDOException $pdoex) {
     header('HTTP/1.1 500 Internal Server Error');
--- ostoslista.sql
+++ ostoslista.sql
@@ -4,8 +4,8 @@ use shoppinglist;
 
 create table item (
     id int primary key auto_increment,
-    descrition varchar(255) not null,
+    description varchar(255) not null,
     amount smallint unsigned not null,
 );
 
-insert into item (descrition,amount) values ('Test item',1);
+insert into item (description,amount) values ('Test item',1);
